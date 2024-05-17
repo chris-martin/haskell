91f50fa0ad6faa4b5ffed66d158deffdf7d0bb14
Chris Martin <chris.martin@renaissance.com>
2024-05-17 15:53:09 -0600

    more zlib

--- ghc-9-6/flake.nix
+++ ghc-9-6/flake.nix
@@ -20,6 +20,7 @@
         };
         devShells = {
           default = pkgs.mkShell {
+            buildInputs = with pkgs; [ zlib ];
             nativeBuildInputs = with packages; [ cabal ghc hls ];
           };
         };
